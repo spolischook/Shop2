e01ba431322eb110b5f9d17cd30f8597d00b3c00
spolischook <spolischook@gmail.com>
2013-01-19 13:44:23 +0200

    Now we can choose existend categories and new categories separately

--- src/Serge/ShopBundle/Form/ProductType.php
+++ src/Serge/ShopBundle/Form/ProductType.php
@@ -5,23 +5,36 @@ namespace Serge\ShopBundle\Form;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+use Doctrine\Common\Collections\ArrayCollection;
 
 class ProductType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
+        $aCat = array();
+        foreach ($options['data']->getCategories() as $cat) {
+            $aCat[] = $cat;
+        }
+        $categoryCollection = new ArrayCollection($aCat);
         $builder
             ->add('name')
             ->add('description')
             ->add('categories', 'collection', array(
-            'type' => new CategoryType(),
+                'type' => new CategoryType(),
 
-            'allow_add' => true,
-            'allow_delete' => true,
-            'by_reference' => false,
+                'allow_add' => true,
+                'allow_delete' => true,
+                'by_reference' => false,
 
-            'prototype' => true,
-            'prototype_name' => 'category__name__'))
+                'prototype' => true,
+                'prototype_name' => 'category__name__'))
+            ->add('existCategories', 'entity', array(
+                'class' => 'ShopBundle:Category',
+                'property' => 'name',
+                'required' => false,
+                'multiple' => true,
+                'data' => $categoryCollection,
+            ))
         ;
     }
 
