d3a50a64844a33383e6b7296282ec2ea94d7622c
spolischook <spolischook@gmail.com>
2013-01-20 20:54:36 +0200

    Installed PagerFanta && FixtureBundle

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -21,6 +21,8 @@ class AppKernel extends Kernel
             new JMS\SecurityExtraBundle\JMSSecurityExtraBundle(),
             new Serge\ShopBundle\ShopBundle(),
             new Knp\Bundle\MenuBundle\KnpMenuBundle(),
+            new \WhiteOctober\PagerfantaBundle\WhiteOctoberPagerfantaBundle(),
+            new \Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
